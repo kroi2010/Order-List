e8b064f10fda33d198aa4bc50d2197076b86d488
tourniquet <admyn3d@gmail.com>
2019-04-05 22:08:20 +0100

    function instead of class

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,17 +3,11 @@ import {connect} from "react-redux";
 import{bindActionCreators} from "redux";
 import{searchOrders} from "../actions"
 
-class Search extends React.Component {
-
-    render() {
-        return(
-            <div className="search_container"> 
-                <input placeholder="Look for..."  onKeyUp={e => this.props.searchOrders(e.target.value)}/>
-                <button className="search_button">Search</button>
-            </div>
-        );
-    }
-}
+const Search = props =>
+  <div className="search_container">
+    <input placeholder="Look for..." onKeyUp={e => props.searchOrders(e.target.value)} />
+    <button className="search_button">Search</button>
+  </div>
 
 const matchDispatchToProps = (dispatch) => bindActionCreators({searchOrders: searchOrders}, dispatch);
 
